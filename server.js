4140c79849523941937078625c7305ef798de642
Deepak Singh <deepaksingh567845@gmail.com>
2026-04-03 19:40:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/study", require("./routes/study"));
 app.use("/api/ai", require("./routes/ai"));
 app.use("/api/weeklyPlan", require("./routes/weeklyPlan"));
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () =>
-  console.log("🚀 Server running at http://localhost:5000")
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
 );
